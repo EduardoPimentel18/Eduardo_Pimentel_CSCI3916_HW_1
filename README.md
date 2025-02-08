97c55a44230498274ad80fbf2bbe7c3378fbc245
EduardoPimentel18 <eduardo.world9@gmail.com>
2025-02-08 15:19:10 -0700

    Modify the server.js file and added the postman button

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Eduardo_Pimentel_CSCI3916_HW_1 
 # 2/8/25
+
+[<img src="https://run.pstmn.io/button.svg" alt="Run In Postman" style="width: 128px; height: 32px;">](https://app.getpostman.com/run-collection/41726845-f124f24f-be4b-4238-9c9a-f8ddc7609b70?action=collection%2Ffork&source=rip_markdown&collection-url=entityId%3D41726845-f124f24f-be4b-4238-9c9a-f8ddc7609b70%26entityType%3Dcollection%26workspaceId%3D403a8607-442e-4472-8515-d58abd2d2dd5)
--- server.js
+++ server.js
@@ -6,10 +6,13 @@ app.use(express.json()); // Middleware to parse JSON request body
 
 // Create a route that listens for GET request on the root URL
 app.post('/', (req, res) => {
-    const acceptHeader = req.get('accept');
+    const acceptHeader = req.get('Accept');
+
+    if(acceptHeader) {
+        res.setHeader('Accept', acceptHeader);
+    }
     
     const responseBody = {
-        acceptHeader: acceptHeader,
         ...req.body
     }
 
