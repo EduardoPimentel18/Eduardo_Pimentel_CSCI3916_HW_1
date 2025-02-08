c232dab0c13995b75bca77518bae2cc0701b9416
EduardoPimentel18 <eduardo.world9@gmail.com>
2025-02-08 14:28:01 -0700

    Modify some files and fix some of the versions of chai

---
+++ server.js
@@ -0,0 +1,25 @@
+// Pulling in express to create the APi server
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON request body
+
+// Create a route that listens for GET request on the root URL
+app.post('/', (req, res) => {
+    const acceptHeader = req.get('accept');
+    
+    const responseBody = {
+        acceptHeader: acceptHeader,
+        ...req.body
+    }
+
+    res.json(responseBody);
+
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log('Server is running on port ${port}');      
+});
+
+module.exports = server;    
